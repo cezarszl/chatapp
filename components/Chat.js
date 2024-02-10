3a92e397a896f8d3748ab8ccdbd5b6715e52d250
Cezary Szal <146932043+cezarszl@users.noreply.github.com>
2024-02-10 21:44:02 +0100

    Task Full-Stack Immersion Exercise 5.2

--- components/Chat.js
+++ components/Chat.js
@@ -1,27 +1,76 @@
-import { useEffect } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { useState, useEffect } from "react";
+import { StyleSheet, View, Platform, KeyboardAvoidingView } from 'react-native';
+import { Bubble, GiftedChat } from "react-native-gifted-chat";
 
 const Chat = ({ route, navigation }) => {
-    const { name, background } = route.params;
+  {/* a meessage state  */}
+  const [messages, setMessages] = useState([]); 
+  const { name, background } = route.params;
 
-    useEffect(() => {
-        navigation.setOptions({ title: name });
-      }, []);
+  
+  useEffect(() => {
+    navigation.setOptions({ title: name })
+    setMessages([
+      {
+        _id: 1,
+        text: 'Hello developer',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any',
+        },
+      },
+      {
+        _id: 2,
+        text: 'This is a system message',
+        createdAt: new Date(),
+        system: true,
+      },
+    ]);
 
-    return (
-        <View style={[styles.container, {backgroundColor: background}]}>
-            <Text>Welcome to the chat</Text>
-        </View>
- );
+  }, []);
+
+  const onSend = (newMessages) => {
+    setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages))
+  }
+
+  const renderBubble = (props) => {
+    return <Bubble
+      {...props}
+      wrapperStyle={{
+        right: {
+          backgroundColor: "#000"
+        },
+        left: {
+          backgroundColor: "#FFF"
+        }
+      }}
+    />
+  }
+
+  return (
+    <View style={[styles.container, {backgroundColor: background}]}>
+      <GiftedChat
+        messages={messages}
+        renderBubble={renderBubble}
+        onSend={messages => onSend(messages)}
+        user={{
+          _id: 1,
+          name
+        }}
+      />
+      {/* in case the keyboard covers the bottom part of start screen  */}
+      {Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null}
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
  container: {
    flex: 1,
-   justifyContent: 'center',
-   alignItems: 'center',
-   width: '100%',
-   height: '100%',
+  width: '100%',
+  
  }
 });
 
